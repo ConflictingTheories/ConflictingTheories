0ffbce7ee870d7344e12fdbd02ce1a9712b70180
kderbyma_ <kyle@kderbyma.com>
2021-01-19 13:01:31 -0700

    adjusting issues

--- Sites/Sovereignty/output/admin/service-worker.js
+++ Sites/Sovereignty/output/admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/admin/precache-manifest.0afcdf1e094c17a6031289284d5f8537.js"
+  "/precache-manifest.5015afbb71a1804d53e1dbbaa8a0c3fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/admin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
