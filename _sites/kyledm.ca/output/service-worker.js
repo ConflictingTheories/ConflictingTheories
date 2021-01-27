eacb81d5a8a6cb63e685bdb4a731bf679eb3fd07
kderbyma_ <kyle@kderbyma.com>
2021-01-26 17:22:45 -0700

    tweak

--- _sites/kyledm.ca/output/service-worker.js
+++ _sites/kyledm.ca/output/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1b3d282c6dfb94eda2a7ae7c6613ee3.js"
+  "/precache-manifest.dc50dc34a3b3a226c40c97f82a160aa3.js"
 );
 
 self.addEventListener('message', (event) => {
