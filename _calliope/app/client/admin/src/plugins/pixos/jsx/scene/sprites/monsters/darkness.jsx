61f0320add89160de74d41bcc165ac6d955d1440
Kyle Derby MacInnis <director@sovereign.enterprises>
2021-11-22 19:33:55 -0700

    sprites

---
+++ _calliope/app/client/admin/src/plugins/pixos/jsx/scene/sprites/monsters/darkness.jsx
@@ -0,0 +1,53 @@
+/*                                                 *\
+** ----------------------------------------------- **
+**             Calliope - Site Generator   	       **
+** ----------------------------------------------- **
+**  Copyright (c) 2020-2021 - Kyle Derby MacInnis  **
+**                                                 **
+**    Any unauthorized distribution or transfer    **
+**       of this work is strictly prohibited.      **
+**                                                 **
+**               All Rights Reserved.              **
+** ----------------------------------------------- **
+\*                                                 */
+
+import { Vector } from "../../../engine/utils/math/vector.jsx";
+import Resources from "../../../engine/utils/resources.jsx";
+export default {
+  // Character art from http://opengameart.org/content/chara-seth-scorpio
+  src: Resources.artResourceUrl("darkness.gif"),
+  sheetSize: [128, 256],
+  tileSize: [24, 32],
+  // Frames & Faces
+  frames: {
+    up: [
+      [0, 0],
+      [24, 0],
+      [48, 0],
+      [24, 0],
+    ],
+    right: [
+      [0, 32],
+      [24, 32],
+      [48, 32],
+      [24, 32],
+    ],
+    down: [
+      [0, 64],
+      [24, 64],
+      [48, 64],
+      [24, 64],
+    ],
+    left: [
+      [0, 96],
+      [24, 96],
+      [48, 96],
+      [24, 96],
+    ],
+  },
+  // Offsets
+  drawOffset: new Vector(-0.25, 1, 0.125),
+  hotspotOffset: new Vector(0.5, 0.5, 0),
+  // Should the camera follow the player?
+  bindCamera: false,
+};
